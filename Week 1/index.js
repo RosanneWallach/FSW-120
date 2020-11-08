5f2112d03f18a43b0b11599636445e2ee65351cc
Rosanne Wallach <66342005+RosanneWallach@users.noreply.github.com>
2020-11-08 01:58:54 -0500

    Add files via upload

---
+++ Week 1/index.js
@@ -0,0 +1,18 @@
+function App()
+return (
+    <div>
+        <input type="checkbox" />
+        <p>Text</p>
+
+        <input type="checkbox" />
+        <p>Text</p>
+
+        <input type="checkbox" />
+        <p>Text</p>
+
+        <input type="checkbox" />
+        <p>Text</p>
+    </div>
+)
+
+}
